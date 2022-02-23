19ba30dd0bc16ac88bf46b3f7454f5a7f751178c
Lucas Fernandes <lucas.fernandes@dataminesoftware.com>
2022-02-23 18:43:09 -0300

    Adicionados arquivos de exercicios sessão 1

---
+++ 01-Fundamentals-Part1/exercises_session1.js
@@ -0,0 +1,6 @@
+let country = "Brazil";
+let continent = "South America";
+let population = 250;
+console.log("Country: ", country);
+console.log("Continent: ", continent);
+console.log("Population: ", population, "M");
